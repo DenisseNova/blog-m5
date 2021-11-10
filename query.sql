2965733fe86a5529ff8a6cea57b2a217dc6dfabf
Denisse Nova <denisse.nova.v@gmail.com>
2021-11-09 21:30:48 -0300

    insert post

--- query.sql
+++ query.sql
@@ -32,3 +32,22 @@ insert into Usuario values(8, 'usuario08@yahoo.com');
 insert into Usuario values(9, 'usuario09@yahoo.com');
 
 select * from Usuario;
+
+insert into Post values(1, 1, 'Post 1: Esto es malo', '2020-06-29');
+insert into Post values(2, 5, 'Post 2: Esto es malo', '2020-06-20');
+insert into Post values(3, 1, 'Post 3: Esto es excelente', '2020-05-30');
+insert into Post values(4, 9, 'Post 4: Esto es bueno', '2020-05-09');
+insert into Post values(5, 7, 'Post 5: Esto es bueno', '2020-07-10');
+insert into Post values(6, 5, 'Post 6: Esto es excelente', '2020-07-18');
+insert into Post values(7, 8, 'Post 7: Esto es excelente', '2020-07-07');
+
+insert into Post values(8, 5, 'Post 8: Esto es excelente', '2020-05-14');
+insert into Post values(9, 2, 'Post 9: Esto es bueno', '2020-05-08');
+insert into Post values(10, 6, 'Post 10: Esto es bueno', '2020-06-02');
+insert into Post values(11, 4, 'Post 11: Esto es bueno', '2020-05-05');
+insert into Post values(12, 9, 'Post 12: Esto es malo', '2020-07-23');
+insert into Post values(13, 5, 'Post 13: Esto es excelente', '2020-05-30');
+insert into Post values(14, 8, 'Post 14: Esto es excelente', '2020-05-01');
+insert into Post values(15, 7, 'Post 15: Esto es malo', '2020-06-17');
+
+select * from Post;
