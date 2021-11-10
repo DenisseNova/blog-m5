0af1c6d262297457713a779f8ef8aa3c85a90924
Denisse Nova <denisse.nova.v@gmail.com>
2021-11-09 21:30:11 -0300

    insert usuario

--- query.sql
+++ query.sql
@@ -21,3 +21,14 @@ create table Comentarios(
 	foreign key(usuario_id) references Usuario(id),
 	foreign key(post_id) references Post(id));
 	
+insert into Usuario values(1, 'usuario01@hotmail.com');
+insert into Usuario values(2, 'usuario02@gmail.com');
+insert into Usuario values(3, 'usuario03@gmail.com');
+insert into Usuario values(4, 'usuario04@hotmail.com');
+insert into Usuario values(5, 'usuario05@yahoo.com');
+insert into Usuario values(6, 'usuario06@hotmail.com');
+insert into Usuario values(7, 'usuario07@yahoo.com');
+insert into Usuario values(8, 'usuario08@yahoo.com');
+insert into Usuario values(9, 'usuario09@yahoo.com');
+
+select * from Usuario;
